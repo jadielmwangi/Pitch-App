48d1baf9b3826c163ca6edc7cb6fea165c813698
jadielmwangi <jedielmwangi@gmail.com>
2020-05-04 22:22:35 +0300

    updates the ProdConfig class by placing the production SQLALCHEMY_DATABASE_URI and setting it to the environment variable DATABASE_URL

--- config.py
+++ config.py
@@ -4,6 +4,23 @@ class Config:
     '''
     General configuration parent class
     '''
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:Access@localhost/pitch'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+    #email configuration
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    SUBJECT_PREFIX = 'pitch'
+    SENDER_EMAIL = 'jedielmwangi@gmail.com'
     
 
     @staticmethod
